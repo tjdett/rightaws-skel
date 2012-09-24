a7b3e830fd3d5b97f7ac26323f7ac98a5779b705
Tim Dettrick <tim@dettrick.net>
2012-09-25 09:10:03 +1000

    Adding monkey patch to access bucket keys.

--- test.rb
+++ test.rb
@@ -4,6 +4,14 @@ Bundler.setup
 require 'uri'
 require 'right_aws'
 
+module RightAws
+  class S3Interface
+    def is_dns_bucket?(bucket_name)
+      false
+    end
+  end
+end
+
 [:S3_URL, :EC2_ACCESS_KEY, :EC2_SECRET_KEY].each do |v|
   raise ArgumentError.new("Need %s env var set." % v) unless ENV.key?(v.to_s)
 end
@@ -18,5 +26,10 @@ s3 = RightAws::S3.new(
     :protocol => server.scheme
   })
 
-puts "Existing buckets: %s" % s3.buckets.map{|b| b.name}.inspect
+s3.buckets.each do |b|
+  puts "%s" % b.name
+  b.keys.each do |k|
+    puts "- %s" % k
+  end
+end
 
